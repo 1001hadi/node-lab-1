a2ffe1267bc8a1190499ce1b71fdc52ff0ff99aa
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-04-11 13:54:13 -0600

    html added and styled with node js

--- server.mjs
+++ server.mjs
@@ -7,8 +7,10 @@ const PORT = 3000;
 http
   .createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello World!\n");
+    res.setHeader("Content-Type", "text/html");
+    res.write(`<h1 style="color:blue">Hello Lab!</h1>`);
+    res.write("<p>I wonder what else we can send...</p>");
+    res.end();
   })
   .listen(PORT, hostname, () => {
     console.log(`Server running at http://${hostname}:${PORT}/`);
