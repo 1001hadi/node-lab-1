80983ed20290f827589d1d632871c456b070080f
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-04-11 13:49:23 -0600

    node server created and tested

---
+++ server.mjs
@@ -0,0 +1,15 @@
+import http from "http";
+// import { hostname } from "os";
+
+const hostname = "127.0.0.1";
+const PORT = 3000;
+
+http
+  .createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello World!\n");
+  })
+  .listen(PORT, hostname, () => {
+    console.log(`Server running at http://${hostname}:${PORT}/`);
+  });
