b05de9abe4a6a70ffe3e87969122aff14e08b6d8
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-04-11 14:06:06 -0600

    last part coded and node.js has been explored and tested

--- server.mjs
+++ server.mjs
@@ -6,11 +6,31 @@ const PORT = 3000;
 
 http
   .createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.write(`<h1 style="color:blue">Hello Lab!</h1>`);
-    res.write("<p>I wonder what else we can send...</p>");
-    res.end();
+    // res.statusCode = 200;
+    // res.setHeader("Content-Type", "text/html");
+    //     res.write(`<h1 style="color:blue">Hello Lab!</h1>`);
+    //     res.write("<p>I wonder what else we can send...</p>");
+    //     res.end();
+    let path = req.url;
+    if (path === "/") {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/html");
+      res.end(`
+            <h1 style="color:teal">Hello to my node Lab!</h1>
+            `);
+    } else if (path == "/test") {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/html");
+      res.end(`
+            <h1 style="color:teal">Welcome to test page!</h1>
+            `);
+    } else {
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/html");
+      res.end(`
+            <h1 style="color:red">Page not found!!!</h1>
+            `);
+    }
   })
   .listen(PORT, hostname, () => {
     console.log(`Server running at http://${hostname}:${PORT}/`);
